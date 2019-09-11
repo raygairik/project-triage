e6e39e55f7738d219557be2e9d0e7f8f9fa6ce77
raygairik <54493787+raygairik@users.noreply.github.com>
2019-09-11 02:16:37 -0400

    Added codes
    Added codes

---
+++ app/public/Patientdummy.php
@@ -0,0 +1,23 @@
+[
+{
+ "patientGuid":"123",
+  "firstName":"John",
+  "lastName":"Doe",
+  "dob":"2012-09-01",
+  "sexAtBirth":"M"
+},
+{
+ "patientGuid":"124",
+  "firstName":"Johnny",
+  "lastName":"Doey",
+  "dob":"2012-09-04",
+  "sexAtBirth":"M"
+},
+{
+ "patientGuid":"125",
+  "firstName":"hanny",
+  "lastName":"Demy",
+  "dob":"2012-09-05",
+  "sexAtBirth":"F"
+}
+]
---
+++ app/public/js/waiting.app.js
@@ -0,0 +1,29 @@
+var waitingApp = new Vue({
+
+  el: '#patientWaitingApp',
+data: {
+  patients: [
+  {
+   "patientGuid":"123",
+    "firstName":"John",
+    "lastName":"Doe",
+    "dob":"2012-09-01",
+    "sexAtBirth":"M"
+  },
+  {
+   "patientGuid":"124",
+    "firstName":"Johnny",
+    "lastName":"Doey",
+    "dob":"2012-09-04",
+    "sexAtBirth":"M"
+  },
+  {
+   "patientGuid":"125",
+    "firstName":"hanny",
+    "lastName":"Demy",
+    "dob":"2012-09-05",
+    "sexAtBirth":"F"
+  }
+  ]
+  }
+});
