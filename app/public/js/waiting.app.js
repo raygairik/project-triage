638029a2b6605dac66a351ca112f5d0f34757e38
raygairik <54493787+raygairik@users.noreply.github.com>
2019-09-12 14:05:48 -0400

    Updated README.TXT, dummy.xl and dummy.json
    Updated README.TXT, dummy.xl and dummy.json

--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,29 +1,49 @@
+/*jshint esversion: 6 */
 var waitingApp = new Vue({
 
   el: '#patientWaitingApp',
 data: {
+
   patients: [
   {
    "patientGuid":"123",
     "firstName":"John",
     "lastName":"Doe",
     "dob":"2012-09-01",
-    "sexAtBirth":"M"
+    "sexAtBirth":"M",
+    "Priority":"High"
   },
   {
    "patientGuid":"124",
     "firstName":"Johnny",
     "lastName":"Doey",
     "dob":"2012-09-04",
-    "sexAtBirth":"M"
+    "sexAtBirth":"M",
+    "Priority":"Low"
   },
   {
    "patientGuid":"125",
     "firstName":"hanny",
     "lastName":"Demy",
     "dob":"2012-09-05",
-    "sexAtBirth":"F"
+    "sexAtBirth":"F",
+    "Priority":"High"
   }
   ]
+},
+methods:{
+  fetchPatients(){
+   fetch('https://randomuser.me/api')
+    .then( response => response.json())
+    .then( json => {waitingApp.patients=json()});
+
+
   }
+},
+created:function(){
+    fetchPatients();
+
+
+
+}
 });
