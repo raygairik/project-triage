4578f2930296c4dff00f5650d538eb7ebe0586cc
raygairik <54493787+raygairik@users.noreply.github.com>
2019-09-15 23:21:39 -0400

    added priority
    added priority

---
+++ app/public/dummy.php
@@ -0,0 +1,37 @@
+[{
+       "patientGuid":"123",
+       "firstName":"John",
+       "lastName":"Doe",
+       "dob":"2012-09-01",
+       "sexAtBirth":"M",
+       "priority":"High"
+
+     },
+  {
+     "patientGuid":"124",
+     "firstName":"Johnny",
+     "lastName":"Doey",
+     "dob":"2012-09-04",
+     "sexAtBirth":"M",
+     "priority":"Medium"
+
+},
+{
+     "patientGuid":"125",
+     "firstName":"Hanny",
+     "lastName":"Dem",
+     "dob":"2012-09-05",
+     "sexAtBirth":"F",
+     "priority":"Low"
+
+},
+{
+    "patientGuid":"126",
+    "firstName":"Lanny",
+    "lastName":"Demy",
+    "dob":"2012-09-05",
+    "sexAtBirth":"F",
+    "priority":"High"
+
+}
+]
--- app/public/js/waiting.app.js
+++ app/public/js/waiting.app.js
@@ -1,49 +1,17 @@
 /*jshint esversion: 6 */
 var waitingApp = new Vue({
-
   el: '#patientWaitingApp',
 data: {
-
-  patients: [
-  {
-   "patientGuid":"123",
-    "firstName":"John",
-    "lastName":"Doe",
-    "dob":"2012-09-01",
-    "sexAtBirth":"M",
-    "Priority":"High"
-  },
-  {
-   "patientGuid":"124",
-    "firstName":"Johnny",
-    "lastName":"Doey",
-    "dob":"2012-09-04",
-    "sexAtBirth":"M",
-    "Priority":"Low"
-  },
-  {
-   "patientGuid":"125",
-    "firstName":"hanny",
-    "lastName":"Demy",
-    "dob":"2012-09-05",
-    "sexAtBirth":"F",
-    "Priority":"High"
-  }
-  ]
+  patients: []
 },
 methods:{
   fetchPatients(){
-   fetch('https://randomuser.me/api')
-    .then( response => response.json())
-    .then( json => {waitingApp.patients=json()});
-
-
+    fetch('dummy.php')
+    .then(response => response.json())
+    .then(json => { waitingApp.patients = json });
   }
 },
-created:function(){
-    fetchPatients();
-
-
-
+created(){
+    this.fetchPatients()
 }
 });
